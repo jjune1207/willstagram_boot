21d13441adf013f98df10f4f87a3c5323b332ac1
jjune1207 <12_sunkist_7@naver.com>
2020-06-02 17:41:49 +0900

    로딩 시도3

--- src/main/resources/static/js/wComments_messages_en.js
+++
@@ -1,8 +0,0 @@
-var msg = {
-		Comment : "Comment",
-		Reply : "Reply",
-		Edit : "Edit",
-		Delete : "Delete",
-		ReComments : "Comment a recomment",
-		Send : "Send"
-};
--- src/main/resources/static/js/wComments_messages_ko.js
+++
@@ -1,8 +0,0 @@
-var msg = {
-		Comment : "댓글",
-		Reply : "댓글",
-		Edit : "수정",
-		Delete : "삭제",
-		ReComment : "내용을 입력하세요.",
-		Send : "쓰기"
-};
